1535e18dc59d14c3ba0aa9e42ac7530f6d0cca13
Nemesai38 <pappysouls@gmail.com>
2023-12-31 19:37:40 +0100

    Third Commit

---
+++ DOMexecs/DOMeventlisteners/Event Delegation/script.js
@@ -0,0 +1,27 @@
+// DOM Manipulation
+
+// Event Delegation
+
+// It allows users to append a SINGLE event listener to a 
+// parent element that adds it to all of its present AND 
+// future descendants that match a selector.
+
+document.querySelector('.sports').addEventListener
+('click', function(e) {
+
+    console.log(e.target.getAttribute('id') + ' is clicked');
+
+    const target = e.target;
+
+    if (target.matches('li')) {
+        target.style.backgroundColor = 'grey';
+    }
+})
+
+const sports = document.querySelector('.sports');
+const newSport = document.createElement('li');
+
+newSport.innerText = 'Rugby';
+newSport.setAttribute('id', 'rugby');
+
+sports.appendChild(newSport);
